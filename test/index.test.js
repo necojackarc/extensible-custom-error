7ed21aae3365a9ccf6a8de25546807ebcd7cb2a3
necojackarc <necojackarc@gmail.com>
2018-10-07 18:23:02 +0000

    :hammer_and_wrench: add test cases when captureStackTrace is unavailable

--- test/index.test.js
+++ test/index.test.js
@@ -8,62 +8,82 @@ describe('ExtensibleCustomError', function() {
   describe('MyError extends ExtensibleCustomError', function() {
     class MyError extends ExtensibleCustomError {}
     const className = 'MyError';
+    const captureStackTrace = Error.captureStackTrace;
 
-    context('given a message', function() {
-      it('should contain name, message, and stack properly', function() {
-        const message = 'message';
-        const myError = new MyError(message);
+    [true, false].forEach((isCaptureStackTraceAvailable) => {
+      const availability =
+        isCaptureStackTraceAvailable ? 'available' : 'unavailable';
 
-        // toString() returns the first line of the stack trace
-        expect(myError.toString()).to.equal(`${className}: ${message}`);
+      context(`when captureStackTrace is ${availability}`, function() {
+        before(function() {
+          if (!isCaptureStackTraceAvailable) {
+            delete Error.captureStackTrace;
+          }
+        });
 
-        expect(myError.name).to.equal(className);
-        expect(myError.message).to.equal(message);
+        after(function() {
+          if (!isCaptureStackTraceAvailable) {
+            Error.captureStackTrace = captureStackTrace;
+          }
+        });
 
-        expect(myError).to.be.an.instanceof(Error);
-        expect(myError).to.be.an.instanceof(ExtensibleCustomError);
-        expect(myError).to.be.an.instanceof(MyError);
-      });
-    });
+        context('given a message', function() {
+          it('should contain name, message, and stack properly', function() {
+            const message = 'message';
+            const myError = new MyError(message);
 
-    context('given an error', function() {
-      it('should contain name, message, and stack properly', function() {
-        const originalMessage = 'originalMessage';
-        const errorToWrap = new Error(originalMessage);
-        const myError = new MyError(errorToWrap);
+            // toString() returns the first line of the stack trace
+            expect(myError.toString()).to.equal(`${className}: ${message}`);
 
-        // toString() returns the first line of the stack trace
-        expect(myError.toString()).to.equal(
-          `${className}: ${errorToWrap.toString()}`
-        );
+            expect(myError.name).to.equal(className);
+            expect(myError.message).to.equal(message);
 
-        expect(myError.name).to.equal(className);
-        expect(myError.message).to.equal(errorToWrap.toString());
-        expect(myError.stack).to.include(errorToWrap.stack);
+            expect(myError).to.be.an.instanceof(Error);
+            expect(myError).to.be.an.instanceof(ExtensibleCustomError);
+            expect(myError).to.be.an.instanceof(MyError);
+          });
+        });
 
-        expect(myError).to.be.an.instanceof(Error);
-        expect(myError).to.be.an.instanceof(ExtensibleCustomError);
-        expect(myError).to.be.an.instanceof(MyError);
-      });
-    });
+        context('given an error', function() {
+          it('should contain name, message, and stack properly', function() {
+            const originalMessage = 'originalMessage';
+            const errorToWrap = new Error(originalMessage);
+            const myError = new MyError(errorToWrap);
 
-    context('given a message and an error', function() {
-      it('should contain name, message, and stack properly', function() {
-        const originalMessage = 'originalMessage';
-        const errorToWrap = new Error(originalMessage);
-        const message = 'message';
-        const myError = new MyError(message, errorToWrap);
+            // toString() returns the first line of the stack trace
+            expect(myError.toString()).to.equal(
+              `${className}: ${errorToWrap.toString()}`
+            );
 
-        // toString() returns the first line of the stack trace
-        expect(myError.toString()).to.equal(`${className}: ${message}`);
+            expect(myError.name).to.equal(className);
+            expect(myError.message).to.equal(errorToWrap.toString());
+            expect(myError.stack).to.include(errorToWrap.stack);
 
-        expect(myError.name).to.equal(className);
-        expect(myError.message).to.equal(message);
-        expect(myError.stack).to.include(errorToWrap.stack);
+            expect(myError).to.be.an.instanceof(Error);
+            expect(myError).to.be.an.instanceof(ExtensibleCustomError);
+            expect(myError).to.be.an.instanceof(MyError);
+          });
+        });
 
-        expect(myError).to.be.an.instanceof(Error);
-        expect(myError).to.be.an.instanceof(ExtensibleCustomError);
-        expect(myError).to.be.an.instanceof(MyError);
+        context('given a message and an error', function() {
+          it('should contain name, message, and stack properly', function() {
+            const originalMessage = 'originalMessage';
+            const errorToWrap = new Error(originalMessage);
+            const message = 'message';
+            const myError = new MyError(message, errorToWrap);
+
+            // toString() returns the first line of the stack trace
+            expect(myError.toString()).to.equal(`${className}: ${message}`);
+
+            expect(myError.name).to.equal(className);
+            expect(myError.message).to.equal(message);
+            expect(myError.stack).to.include(errorToWrap.stack);
+
+            expect(myError).to.be.an.instanceof(Error);
+            expect(myError).to.be.an.instanceof(ExtensibleCustomError);
+            expect(myError).to.be.an.instanceof(MyError);
+          });
+        });
       });
     });
   });
